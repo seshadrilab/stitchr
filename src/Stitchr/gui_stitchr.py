36849b36ed61e25e023fc8095d4ae711c80e8856
Thomas <tmixon@uw.edu>
2023-12-12 14:43:49 -0800

    Fix linked error from missing leader sequence

--- src/Stitchr/gui_stitchr.py
+++ src/Stitchr/gui_stitchr.py
@@ -613,7 +613,7 @@ def main():
                 if warning_msgs['linked_out']:
                     window['linked_log'].update(warning_msgs['linked_out'])
 
-                if seamless == False:
+                elif seamless == False:
                     sd.display(outputs['linked'], parts, fxn.translate_nt(outputs['linker_seq']))
             # Re-enable stitchr button once completed
             window['Run Stitchr'].update(disabled=False)
